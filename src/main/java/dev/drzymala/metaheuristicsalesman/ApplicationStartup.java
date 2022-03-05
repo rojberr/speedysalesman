ff93f0e47f8fd52cac0665224bc916c4c4b8fffd
Rojberr <rojberr@outlook.com>
2022-03-05 10:54:35 +0100

    feat(): Add Solution package with Point as basic Entity

---
+++ src/main/java/dev/drzymala/metaheuristicsalesman/ApplicationStartup.java
@@ -0,0 +1,16 @@
+package dev.drzymala.metaheuristicsalesman;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class ApplicationStartup implements CommandLineRunner {
+
+    public ApplicationStartup() {
+
+    }
+
+    @Override
+    public void run(String ... args) {
+
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/dev/drzymala/metaheuristicsalesman/solution/Point.java
@@ -0,0 +1,17 @@
+package dev.drzymala.metaheuristicsalesman.solution;
+
+import lombok.Value;
+
+@Value
+public class Point {
+
+    private Long x;
+    private Long y;
+
+    public double distanceToPoint(Point point) {
+
+        Long x = Math.abs(getX() - point.getX());
+        Long y = Math.abs(getY() - point.getY());
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+}
