294e11a988683d965036e5fd73159b01180463f2
Rojberr <rojberr@outlook.com>
2022-03-05 10:21:46 +0100

    Init commit

---
+++ src/main/java/dev/drzymala/metaheuristicsalesman/MetaheuristicsalesmanApplication.java
@@ -0,0 +1,13 @@
+package dev.drzymala.metaheuristicsalesman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaheuristicsalesmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaheuristicsalesmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drzymala/metaheuristicsalesman/MetaheuristicsalesmanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drzymala.metaheuristicsalesman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaheuristicsalesmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
