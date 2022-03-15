f165d55816be2ef77ce8c6942de756b65198f81e
Rojberr <rojberr@outlook.com>
2022-03-15 23:02:49 +0100

    feat(present): Add custom Error Controller and error page

---
+++ src/main/java/dev/drzymala/speedysalesman/presenter/web/CustomErrorController.java
@@ -0,0 +1,17 @@
+package dev.drzymala.speedysalesman.presenter.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        return "error";
+    }
+}
