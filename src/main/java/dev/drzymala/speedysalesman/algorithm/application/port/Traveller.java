d9faad73377dff88cf23a5ae694cc7b1d3b4ffed
Rojberr <rojberr@outlook.com>
2022-03-16 22:46:36 +0100

    feat(th): Make button on error.html redirect to index.html

--- src/main/java/dev/drzymala/speedysalesman/algorithm/application/port/Traveller.java
+++ src/main/java/dev/drzymala/speedysalesman/algorithm/application/port/Traveller.java
@@ -1,7 +1,6 @@
 package dev.drzymala.speedysalesman.algorithm.application.port;
 
 import dev.drzymala.speedysalesman.algorithm.domain.City;
-import dev.drzymala.speedysalesman.algorithm.domain.CityRepository;
 
 import java.util.List;
 
