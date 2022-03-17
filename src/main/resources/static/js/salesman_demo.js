993c9a31216b59eb6929eacfeb5e1c184d7af94b
Rojberr <rojberr@outlook.com>
2022-03-17 20:32:57 +0100

    refac(): Remove animation from index.html because it's awful

--- src/main/resources/static/js/salesman_demo.js
+++
@@ -1,6 +0,0 @@
-// var canvas = document.getElementById("canvas");
-// var ctx = canvas.getContext("2d");
-//
-// // ctx.fillStyle = "#000000";
-// // ctx.fillRect(0, 0, 400, 200);
-// gsap.timeline();
