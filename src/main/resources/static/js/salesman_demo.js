a29790c90b429f58108097f559ec1e5d9fd96553
Rojberr <rojberr@outlook.com>
2022-03-17 20:29:04 +0100

    feat(index.html): Try to make a small animation, resign, and add link to one

---
+++ src/main/resources/static/js/salesman_demo.js
@@ -0,0 +1,6 @@
+// var canvas = document.getElementById("canvas");
+// var ctx = canvas.getContext("2d");
+//
+// // ctx.fillStyle = "#000000";
+// // ctx.fillRect(0, 0, 400, 200);
+// gsap.timeline();
