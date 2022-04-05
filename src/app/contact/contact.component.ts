46523da094020294c3c8ab66ba243e1a42684257
rojberr@outlook.com <rojberr@outlook.com>
2022-04-05 16:31:56 +0200

    feat(contact): Bind contact form to submitForm() and make it clickable only if name and email are correct

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  name: any;
+  email: any;
+  message: any;
+
   constructor() { }
 
   ngOnInit(): void {
-
     // create api call
     // create the form based on api fields
-
   }
 
+  submitForm() {
+    const message = `My name is ${this.name}, 
+                    My emails is ${this.email},
+                    My message is ${this.message}`;
+    // grab all the firlds and their values
+    // const nameInput = document.querySelector('input[name=name]').value;
+    alert(message);
+  }
 }
