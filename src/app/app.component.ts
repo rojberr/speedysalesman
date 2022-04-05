94cb905b5df86c5c742a44e1a142188f0f6b0c3f
Rojberr <rojberr@outlook.com>
2022-04-05 20:44:25 +0200

    feat(): Add Github API call on / page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'speedysalesman';
+  // title = 'speedysalesman';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.http.get('https://api.github.com/users?per_page=10')
+    .subscribe(data => {
+      console.log(data);
+    })
+  }
 }
