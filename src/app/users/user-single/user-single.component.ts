8a2cd7c8e608790a478d755e1a0a724a15056c2e
Rojberr <rojberr@outlook.com>
2022-04-05 22:20:50 +0200

    feat(): Make userlist clickable and show user avatar on each click

--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/user.service';
 
 @Component({
@@ -9,17 +10,20 @@ import { UserService } from 'src/app/user.service';
 export class UserSingleComponent implements OnInit {
   user: any;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // grab the username out of the url
+      this.route.params.subscribe(params => {
+        const username = params['username'];
 
     // use the userservice to get data from github api
     this.userService
-      .getUser('ezmobius')
+      .getUser(username)
       .subscribe(user => this.user = user);
-
     //bind that to a user variable
+    });
   }
-
 }
