de23e130f991bfe1f615437f6b960dc8a96b2006
Rojberr <rojberr@outlook.com>
2022-04-07 22:04:40 +0200

    feat(pathfinder): Add POST point /pathfinder

--- src/main/java/dev/drzymala/speedysalesman/receiver/PathfinderController.java
+++ src/main/java/dev/drzymala/speedysalesman/receiver/PathfinderController.java
@@ -1,6 +1,21 @@
 package dev.drzymala.speedysalesman.receiver;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequestMapping("/pathfinder")
+@RestController
+@AllArgsConstructor
 public class PathfinderController {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void findPath(@RequestBody String data) {
 
+        log.info("Received data " + data);
+        System.out.print(data);
+    }
 }
