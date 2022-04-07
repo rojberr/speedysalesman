ec857a73ae42d25a0ebd395de4fc7beae3b652f1
Rojberr <rojberr@outlook.com>
2022-04-07 20:47:24 +0200

    feat(pathfinder): Add form to /pathfinder

--- src/app/pathfinder/pathfinder.component.ts
+++ src/app/pathfinder/pathfinder.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pathfinder',
   templateUrl: './pathfinder.component.html',
-  styleUrls: ['./pathfinder.component.css']
+  styleUrls: ['./pathfinder.component.css'],
 })
 export class PathfinderComponent implements OnInit {
+  data: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitForm() {
+    const message = `My name is ${this.data}`;
+    // grab all the firlds and their values
+    // const nameInput = document.querySelector('input[name=name]').value;
+    alert(message);
   }
-
 }
