2b5f1e0f9173c484e0368a80e9bc8894c79675ae
rojberr <rojberr@outlook.com>
2022-05-27 23:25:57 +0200

    refac(web): Rename PathfinderController to GreedyController

---
+++ src/main/java/dev/drzymala/speedysalesman/pathfinder/web/GreedyController.java
@@ -0,0 +1,38 @@
+package dev.drzymala.speedysalesman.pathfinder.web;
+
+import dev.drzymala.speedysalesman.pathfinder.application.port.GreedyServiceUseCase;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequestMapping("/greedy")
+@RestController
+@AllArgsConstructor
+public class GreedyController<T> {
+
+    private final GreedyServiceUseCase greedyService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Object findGreedyPath(@RequestBody String data) {
+
+        log.info("Received data " + data);
+        return greedyService
+                .findPath(data)
+                .handle(
+                        cities -> new ResponseEntity(cities, HttpStatus.OK),
+                        error -> ResponseEntity.badRequest().body(error)
+                );
+    }
+
+    @PostMapping("/generate")
+    @ResponseStatus(HttpStatus.OK)
+    public Object getGenerated(@RequestBody int numberOfGenerated) {
+
+        log.info("Generating " + numberOfGenerated + " cities");
+        return greedyService.generate(numberOfGenerated);
+    }
+}
