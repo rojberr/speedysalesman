95ca0c91e747a2a83e5cda850bc27971485e6878
Rojberr <rojberr@outlook.com>
2022-06-03 19:32:21 +0200

    feat(): View generated JSON response in the form

--- src/app/pathfinder/pathfinder.component.ts
+++ src/app/pathfinder/pathfinder.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { PathfinderService } from '../core/services/pathfinder.service';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PathfinderService } from '../core/services/pathfinder.service';
 
 @Component({
   selector: 'app-pathfinder',
   templateUrl: './pathfinder.component.html',
   styleUrls: ['./pathfinder.component.css'],
 })
-export class PathfinderComponent implements OnInit {
+export class PathfinderComponent {
   data: Observable<any[]> | undefined;
 
+  public dataSize: number | undefined;
   public foundPathResponse = [];
+  public generatedResponse = [];
 
   constructor(private input: PathfinderService) {}
 
-  ngOnInit(): void {}
-
   calculateGreedyPath() {
-    alert(message);
-
     this.input.postGreedyPath(this.data).subscribe((result: any) => {
       console.log(result);
       this.foundPathResponse = result;
@@ -37,7 +35,19 @@ export class PathfinderComponent implements OnInit {
     var numberOfGenerated = parseInt(x !== null ? x : '');
 
     this.input.generateData(numberOfGenerated).subscribe((result: any) => {
+      this.generatedResponse = result;
       console.log(JSON.stringify(result));
     });
+  }
+
+  parseGeneratedData(dataToParse: any) {
+    const result = dataToParse
+      .replace('[', '')
+      .replaceAll('{"id":', '')
+      .replaceAll(',"x":', ' ')
+      .replaceAll(',"y":', ' ')
+      .replaceAll('},', '\n')
+      .replace('}]', '');
+    return result.split('\n').length + '\n' + result;
   }
 }
